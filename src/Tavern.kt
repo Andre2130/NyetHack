75971b29677585fefb072872d34af9e1521d94ec
Andre2130 <atrobinson30@gmail.com>
2018-05-05 15:28:58 -0400

    Added Dragon speak function

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,11 +1,48 @@
 const val tavernName = "Taernyl's Folly"
 
+var playerGold = 10
+var playerSilver = 10
+
 fun main(args: Array<String>) {
-    placeOrder()
+    placeOrder("shady, Dragon's Breath, 5.91")
 }
 
-private fun placeOrder() {
+fun performPurchase(){
+    displayBalance()
+}
+
+fun displayBalance() {
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
+}
+
+private fun placeOrder(menuData: String) {
     val indexOfApostrophe = tavernName.indexOf('\'')
     val tavernMaster = tavernName.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    performPurchase()
+
+    var phrase = if(name == "Dragon's Breath"){
+        "Madrigal exclaims ${toDradgonSpeak("Ah, delicious $name!")}"
+    }else{
+        "Madrigal says: Thanks for the $name"
+    }
+    println(phrase)
+}
+
+fun toDradgonSpeak(phrase: String)=
+        phrase.replace(Regex("[aeiou]")){
+
+            when(it.value){
+                "a"->"4"
+                "e"->"3"
+                "i"->"1"
+                "o"->"0"
+                "u"->"|_|"
+                else -> it.value
+            }
 }
